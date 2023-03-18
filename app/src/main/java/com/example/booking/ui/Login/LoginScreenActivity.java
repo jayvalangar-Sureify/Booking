5a633609fe92c7e800831b51a2e0601a84b492e8
JAY-VALANGAR <9valangar0@gmail.com>
2023-03-18 21:11:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/booking/ui/Login/LoginScreenActivity.java
@@ -0,0 +1,18 @@
+package com.example.booking.ui.Login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.booking.R;
+
+public class LoginScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/booking/ui/SplashScreen/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.booking.ui.SplashScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.booking.MainActivity;
+import com.example.booking.R;
+import com.example.booking.ui.Login.LoginScreenActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //will hide the title
+        getSupportActionBar().hide(); // hide the title bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
+
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashScreen.this, LoginScreenActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+
+    }
+}
