bc79f80cd4a57f77e73679607cd081df9e72468c
JAY-VALANGAR <9valangar0@gmail.com>
2023-03-20 16:46:39 +0530

    Signup Screen Added

--- app/src/main/java/com/example/booking/ui/Login/LoginScreenActivity.java
+++ app/src/main/java/com/example/booking/ui/Login/LoginScreenActivity.java
@@ -1,17 +1,34 @@
 package com.example.booking.ui.Login;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.example.booking.R;
+import com.example.booking.databinding.ActivityLoginScreenBinding;
+import com.example.booking.ui.Signup.SignupScreen_User;
 
 public class LoginScreenActivity extends AppCompatActivity {
+
+    ActivityLoginScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        binding = ActivityLoginScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.loginTvNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginScreenActivity.this, SignupScreen_User.class);
+                startActivity(i);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/booking/ui/Signup/SignupScreen_User.java
+++ app/src/main/java/com/example/booking/ui/Signup/SignupScreen_User.java
@@ -1,16 +1,33 @@
 package com.example.booking.ui.Signup;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.booking.R;
+import com.example.booking.databinding.ActivitySignupScreenUserBinding;
+import com.example.booking.ui.Login.LoginScreenActivity;
 
 public class SignupScreen_User extends AppCompatActivity {
+
+    ActivitySignupScreenUserBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen_user);
+
+        binding = ActivitySignupScreenUserBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.signupTvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignupScreen_User.this, LoginScreenActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
