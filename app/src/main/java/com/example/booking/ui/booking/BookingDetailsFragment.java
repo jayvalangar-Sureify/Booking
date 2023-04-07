f010c7cee6838fbb07cb8b2a4fd4a2373c80d467
JAY-VALANGAR <9valangar0@gmail.com>
2023-04-07 17:45:08 +0530

    Owner data applied successfully

--- app/src/main/java/com/example/booking/ui/booking/BookingDetailsFragment.java
+++ app/src/main/java/com/example/booking/ui/booking/BookingDetailsFragment.java
@@ -52,6 +52,7 @@ public class BookingDetailsFragment extends Fragment {
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 
+        binding.progressbarBookingDetailsFragment.setVisibility(View.VISIBLE);
 
         firebaseFirestore.collection(Utils.key_place_booking_firestore)
                 .get()
@@ -62,94 +63,12 @@ public class BookingDetailsFragment extends Fragment {
                             List<DocumentSnapshot> documentSnapshots = task.getResult().getDocuments();
                             getAllBookingIdData(documentSnapshots);
 
-                            int lastIndex = task.getResult().size() - 1;
-//                            for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-//
-//                                String booking_id = documentSnapshot.getId();
-//
-////                                getAllBookingIdData(documentSnapshot);
-//
-//                                Log.i("test_index_response", ""+documentSnapshots.indexOf(documentSnapshot));
-//
-//
-//                                // Check if this is the last iteration
-//                                if (documentSnapshots.indexOf(documentSnapshot) == lastIndex) {
-//                                    is_data_taken_from_server = true;
-//                                }
-//
-//                            }
-
                         } else {
                             Log.i("test_response", "Error getting documents: ", task.getException());
                         }
                     }
                 });
 
-
-//        //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-==-=-=
-//        // Check owner already added place or not
-//        firebaseFirestore.collection(Utils.key_place_booking_firestore)
-//                .get()
-//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                        if (task.isSuccessful()) {
-//
-//
-//                            for (QueryDocumentSnapshot document : task.getResult()) {
-//                                String booking_id = document.getId();
-//
-//                                // Access the nested HashMap
-//                                HashMap<String, HashMap<String, HashMap<String, String>>> bookings_data_hashmap = (HashMap<String, HashMap<String, HashMap<String, String>>>) document.getData().values().iterator().next();
-//
-//                                Log.i("test_response", "SIZE : "+bookings_data_hashmap.size());
-//                                for (String time_slots_key : bookings_data_hashmap.keySet()) {
-//
-//                                    bookings_data_hashmap.get(time_slots_key);
-//
-//                                    String first_hashmap_key = time_slots_key;
-//
-//                                    Log.i("test_response", "first_hashmap_key : "+first_hashmap_key);
-//
-//
-//                                    // Get the second-level HashMap for this key
-//                                    HashMap<String, HashMap<String, String>> time_slots_hashmap = bookings_data_hashmap.get(time_slots_key);
-//
-//                                    // Iterate over each key in the second-level HashMap
-//                                    for (String userid_as_a_key : time_slots_hashmap.keySet()) {
-////                                        // Get the innermost HashMap for this key
-////                                        HashMap<String, String> innerMap = time_slots_hashmap.get(userid_as_a_key);
-////
-////                                        // Iterate over each key in the innermost HashMap
-////                                        for (String innerKey : innerMap.keySet()) {
-////                                            // Get the value for this key
-////                                            String all_details_value = innerMap.get(innerKey);
-////
-////                                            // Do something with the key and value
-////                                            System.out.println(time_slots_key + ", " + userid_as_a_key + ", " + innerKey + ", " + all_details_value);
-////
-////
-////
-////
-//                                        Log.i("test_response", "userid_as_a_key : "+userid_as_a_key);
-//                                        Log.i("test_response", "userid_value : "+time_slots_hashmap.get(userid_as_a_key));
-////                                        Log.i("test_response", "innerKey : "+innerKey);
-////                                        Log.i("test_response", "all_details_value : "+all_details_value);
-//                                    }
-//                                }
-//
-//                            }
-//                        } else {
-//                            Log.i("test_response", "Error getting documents: ", task.getException());
-//                        }
-//                    }
-//                }).addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.i("test_response", "Error getting documents: "+e.getMessage());
-//                    }
-//                });
-//        //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-==-=-=
 
             return root;
     }
@@ -160,53 +79,68 @@ public class BookingDetailsFragment extends Fragment {
     private void getAllBookingIdData(List<DocumentSnapshot> documentSnapshot) {
         //------------------------------------------------------------------------------------
        for(int i = 0; i < documentSnapshot.size(); i++) {
-           Map<String, Object> bookingData = documentSnapshot.get(i).getData();
-           for (Map.Entry<String, Object> bookingEntry : bookingData.entrySet()) {
-               // Get the booking time slot
-               String timeSlot = bookingEntry.getKey();
-               Log.d("TAG", "Time slot: " + timeSlot);
+           String owner_id_with_date_string = documentSnapshot.get(i).getId();
 
-               // Get the nested booking data
-               Map<String, Object> bookingDetails = (Map<String, Object>) bookingEntry.getValue();
-               for (Map.Entry<String, Object> bookingDetailsEntry : bookingDetails.entrySet()) {
-                   // Get the booking ID
-                   String bookingId = bookingDetailsEntry.getKey();
-                   Log.d("test_special_response", "USERRRRRR_IDDDDDDDDDDDD :- " + bookingId);
+           String[] parts = owner_id_with_date_string.split("_");
+
+           String owner_id_from_Server_string = parts[0]; // "15GeAx8pikQFJwFZ5q4GJIXLuMW2"
+           String owner_booking_date = parts[1]; // "05 Apr 2023"
+
+           String get_type_of_login = Utils.get_SharedPreference_type_of_login(getActivity());
+
+           if(get_type_of_login.contains(Utils.owner)){
+               //-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=
+               String current_owner_id = user_Id_string;
+
+               if(current_owner_id.contains(owner_id_from_Server_string)) {
+                   Map<String, Object> bookingData = documentSnapshot.get(i).getData();
+                   for (Map.Entry<String, Object> bookingEntry : bookingData.entrySet()) {
+                       // Get the booking time slot
+                       String timeSlot = bookingEntry.getKey();
+                       Log.d("TAG", "Time slot: " + timeSlot);
+
+                       // Get the nested booking data
+                       Map<String, Object> bookingDetails = (Map<String, Object>) bookingEntry.getValue();
+                       for (Map.Entry<String, Object> bookingDetailsEntry : bookingDetails.entrySet()) {
+                           // Get the booking ID
+                           String bookingId = bookingDetailsEntry.getKey();
+
+                           // Get the nested booking data
+                           Map<String, Object> booking = (Map<String, Object>) bookingDetailsEntry.getValue();
+
+                               loggedIn_user_data_linked_hashmap.put(timeSlot, booking);
+
+                       }
+                   }
+               }
+               //-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=
+           }else{
+               //-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=
+               Map<String, Object> bookingData = documentSnapshot.get(i).getData();
+               for (Map.Entry<String, Object> bookingEntry : bookingData.entrySet()) {
+                   // Get the booking time slot
+                   String timeSlot = bookingEntry.getKey();
+                   Log.d("TAG", "Time slot: " + timeSlot);
 
                    // Get the nested booking data
-                   Map<String, Object> booking = (Map<String, Object>) bookingDetailsEntry.getValue();
+                   Map<String, Object> bookingDetails = (Map<String, Object>) bookingEntry.getValue();
+                   for (Map.Entry<String, Object> bookingDetailsEntry : bookingDetails.entrySet()) {
+                       // Get the booking ID
+                       String bookingId = bookingDetailsEntry.getKey();
 
+                       // Get the nested booking data
+                       Map<String, Object> booking = (Map<String, Object>) bookingDetailsEntry.getValue();
 
-                   Log.d("sajahfsal", "user_Id_string = " + user_Id_string);
-                   Log.d("sajahfsal", "bookingId = " + bookingId);
-
-                   if (user_Id_string.contains(bookingId)) {
-                       loggedIn_user_data_linked_hashmap.put(timeSlot, booking);
+                       if (user_Id_string.contains(bookingId)) {
+                           loggedIn_user_data_linked_hashmap.put(timeSlot, booking);
+                       }
                    }
-
-
-                   // Get the booking fields
-                   String userId = (String) booking.get("key_booking_user_id");
-                   String placeName = (String) booking.get("key_booking_place_name");
-                   Double latitude = Double.valueOf((String) booking.get("key_booking_place_latitude"));
-                   Double longitude = Double.valueOf((String) booking.get("key_booking_place_longitude"));
-                   String staffNumber = (String) booking.get("key_booking_staff_number");
-                   String placeAddress = (String) booking.get("key_booking_place_address");
-                   String bookingDate = (String) booking.get("key_booking_date");
-
-
-                   Log.d("test_response", user_Id_string + " == " + userId);
-
-                   // Log the booking fields
-                   Log.d("TAG", "User ID: " + userId);
-                   Log.d("TAG", "Place Name: " + placeName);
-                   Log.d("TAG", "Latitude: " + latitude);
-                   Log.d("TAG", "Longitude: " + longitude);
-                   Log.d("TAG", "Staff Number: " + staffNumber);
-                   Log.d("TAG", "Place Address: " + placeAddress);
-                   Log.d("TAG", "Booking Date: " + bookingDate);
                }
+               //-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=
            }
+
+
+
        }
 
         //------------------------------------------------------------------------------------------
@@ -220,7 +154,7 @@ public class BookingDetailsFragment extends Fragment {
         //------------------------------------------------------------------------------------------
 
 
-
+        binding.progressbarBookingDetailsFragment.setVisibility(View.GONE);
         //------------------------------------------------------------------------------------
     }
     //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
