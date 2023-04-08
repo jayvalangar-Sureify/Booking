aaf5d5c1afe9c8c3eef61738b205dd46d2a11495
JAY-VALANGAR <9valangar0@gmail.com>
2023-04-08 20:39:38 +0530

    Onclick location nicon show location direction

--- app/src/main/java/com/example/booking/adapter/BookingDetailsAdapter.java
+++ app/src/main/java/com/example/booking/adapter/BookingDetailsAdapter.java
@@ -1,11 +1,20 @@
 package com.example.booking.adapter;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.booking.R;
@@ -87,15 +96,56 @@ public class BookingDetailsAdapter extends RecyclerView.Adapter<BookingDetailsAd
              holder.tv_booking_details_place_address.setText(value);
             }
             if(key.contains(Utils.key_booking_place_latitude)){
-
+            holder.tv_latitude_longitude_hide.setText(value);
             }
             if(key.contains(Utils.key_booking_place_longitude)){
-
+            holder.tv_latitude_longitude_hide.setText(holder.tv_latitude_longitude_hide.getText().toString()+","+value.toString());
             }
 
         }
 
 
+        holder.cv_booking_details_location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(holder.itemView.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//                    ActivityCompat.requestPermissions(getContext(), new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, 1);
+                }
+
+                double latitude = 0;
+                double longitude = 0;
+                LocationManager locationManager = (LocationManager) holder.itemView.getContext().getSystemService(Context.LOCATION_SERVICE);
+
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+                    if (location != null) {
+                         latitude = location.getLatitude();
+                         longitude = location.getLongitude();
+
+                        // Do something with latitude and longitude values
+
+
+                        String uri = "http://maps.google.com/maps?saddr=" + latitude + "," + longitude + "&daddr=" + holder.tv_latitude_longitude_hide.getText().toString();
+                        Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+                        intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+                        holder.itemView.getContext().startActivity(intent);
+                    }
+                } else {
+                    // GPS is disabled, show a dialog to the user to enable it
+                }
+
+
+            }
+        });
+
+
+        holder.cv_booking_details_staff_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
@@ -107,7 +157,9 @@ public class BookingDetailsAdapter extends RecyclerView.Adapter<BookingDetailsAd
         public TextView tv_booking_details_place_time_slots;
         public TextView tv_booking_details_place_address;
         public TextView tv_booking_details_staff_number;
+        public TextView tv_latitude_longitude_hide;
         public TextView tv_date_day, tv_date_month, tv_date_year;
+        public CardView cv_booking_details_location, cv_booking_details_staff_contact;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -115,9 +167,12 @@ public class BookingDetailsAdapter extends RecyclerView.Adapter<BookingDetailsAd
             tv_booking_details_place_time_slots = (TextView) itemView.findViewById(R.id.tv_booking_details_place_time_slots);
             tv_booking_details_place_address = (TextView) itemView.findViewById(R.id.tv_booking_details_place_address);
             tv_booking_details_staff_number = (TextView) itemView.findViewById(R.id.tv_booking_details_staff_number);
+            tv_latitude_longitude_hide = (TextView) itemView.findViewById(R.id.tv_latitude_longitude_hide);
             tv_date_day = (TextView) itemView.findViewById(R.id.tv_date_day);
             tv_date_month = (TextView) itemView.findViewById(R.id.tv_date_month);
             tv_date_year = (TextView) itemView.findViewById(R.id.tv_date_year);
+            cv_booking_details_location = (CardView) itemView.findViewById(R.id.cv_booking_details_location);
+            cv_booking_details_staff_contact = (CardView) itemView.findViewById(R.id.cv_booking_details_staff_contact);
 
         }
     }
