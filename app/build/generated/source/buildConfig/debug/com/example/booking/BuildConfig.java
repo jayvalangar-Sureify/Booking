31d6aa89531ec87f8e6b16196c58a02f9603e969
JAY-VALANGAR <9valangar0@gmail.com>
2023-05-03 22:11:17 +0530

    Application ID changed, package name changed, google-service package name changed

--- app/build/generated/source/buildConfig/debug/com/example/booking/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/example/booking/BuildConfig.java
@@ -5,8 +5,8 @@ package com.example.booking;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.booking";
+  public static final String APPLICATION_ID = "com.valangar.booking";
   public static final String BUILD_TYPE = "debug";
-  public static final int VERSION_CODE = 1;
+  public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "1.0";
 }
